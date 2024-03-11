2622c30128901362a7e4fa06fd998de8c2aac59f
ABoba <a.boba@yandex.ru>
2024-03-11 12:06:20 +0300

    1

---
+++ WindowsFormsAppAntonov/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppAntonov
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Button btn = new Button();
+            btn.Parent = this;
+            btn.Location = new Point(e.X, e.Y);
+            btn.Size = new Size(70, 30);
+            btn.Text = $"{e.X}; {e.Y}";
+            btn.Click += new System.EventHandler(btn_Click);
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            this.Controls.Remove(sender as Button); 
+        }
+    }
+}
